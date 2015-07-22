033ee3929f9fbbbd92e5d9f7d5bf72b8e2465bfb
RobertZK <technoguyrob@gmail.com>
2015-07-22 15:59:16 -0500

    Fix globals controller.

--- lib/controllers/globals.R
+++ lib/controllers/globals.R
@@ -1,10 +1,9 @@
-
-preprocessor <- function(source_args, source, args) {
-  director <- args$director %||% director
-  if (base::exists('..director_inject', envir = parent.env(source_args$local), inherits = FALSE))
-    parent.env(source_args$local)$director <- director
-  else source_args$local$director <- director
+preprocessor <- function(source_env, director, source) {
+  source_env$director <- director
   source()
 }
 
-function() input
+function(input) {
+  input
+}
+
