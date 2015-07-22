0d98dd7eab01319a2b8e74982231813d964ab1a3
RobertZK <technoguyrob@gmail.com>
2015-07-22 16:00:25 -0500

    Fix mungebits controller

--- lib/controllers/mungebits.R
+++ lib/controllers/mungebits.R
@@ -1,12 +1,12 @@
 # This is the mungebits controller.
 
-preprocessor <- function(source_args, source) {
-  parent.env(source_args$local) <- globalenv()
+preprocessor <- function(source_env, source) {
+  parent.env(source_env) <- globalenv()
   source()
 }
 
 function(input, director) {
-  simple_deflate <- director$.cache$engines$modeling$resource("lib/shared/simple_deflate")$value()
+  simple_deflate <- director$resource("lib/shared/simple_deflate")
   mungebits:::mungebit(simple_deflate(input$train), simple_deflate(input$predict))
 }
 
