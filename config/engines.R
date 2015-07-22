17e75c3c906099a3fabe4f916e37d549f64f5fb8
RobertZK <technoguyrob@gmail.com>
2015-07-22 15:55:03 -0500

    Correct director v3 usage in engines file

--- config/engines.R
+++ config/engines.R
@@ -3,7 +3,7 @@ engine("base", type = "local", path = "~/dev/base.sy")
 
 .onAttach <- function(parent_engine) {
   # `director` is the base engine object.
-  routes <- director$.cache$engines$base$resource("lib/controllers/routes")$value()
+  routes <- director$cache_get("engines")$base$resource("lib/controllers/routes")
 
   parent_engine$register_preprocessor("config/routes", routes$preprocessor, overwrite = TRUE)
   parent_engine$register_parser("config/routes", routes$parser, overwrite = TRUE)
@@ -11,22 +11,18 @@ engine("base", type = "local", path = "~/dev/base.sy")
   # TODO: (RK) Fix this hack using a proper helper resource.
   environment(routes$preprocessor)$mount(parent_engine)(director)
 
-  parent_engine$resource("config/routes")$value()
-
-  if (interactive()) {
-    Ramd::packages("crayon")
-  }
+  parent_engine$resource("config/routes")
 
   # Annoying hotfix for RCurl onLoad option setting interfering with tests.
   options(httr_oauth_cache = NULL, httr_oob_default = NULL)
 
-  globals <- director$resource('config/global')$value(director = parent_engine)
+  globals <- director$resource('config/global', director = parent_engine)
   for (global in ls(globals)) {
     assign(global, globals[[global]], envir = globalenv()) # yuck
   }
 
   if (parent_engine$exists('config/global')) {
-    globals <- parent_engine$resource('config/global')$value()
+    globals <- parent_engine$resource('config/global')
     for (global in ls(globals)) {
       assign(global, globals[[global]], envir = globalenv()) # yuck
     }
