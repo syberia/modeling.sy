b122b23239d3bdd3217306eb904354e448e6b111
robertzk <technoguyrob@gmail.com>
2015-08-30 16:24:24 -0500

    Add %||% operator for now to avoid R 3.2.1 problems

--- lib/shared/default_adapter.R
+++ lib/shared/default_adapter.R
@@ -2,6 +2,7 @@
 # \code{"config/application.R"} has a \code{default_adapter} defined,
 # it will be that; otherwise, it will be the file adapter.
 local({
+  `%||%` <- function(x, y) if (is.null(x)) y else x
   default_value <- NULL
   function() {
     default_value <<- default_value %||%
