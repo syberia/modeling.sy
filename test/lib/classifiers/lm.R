1fbe700998f5c74ed3ccdc930dbdfa2f656af06e
RobertZK <technoguyrob@gmail.com>
2015-11-16 14:30:41 -0600

    Check that lm predictions work as expected.

--- test/lib/classifiers/lm.R
+++ test/lib/classifiers/lm.R
@@ -3,5 +3,20 @@ context("lm classifier")
 
 test_that("it can create a simple lm tundra container", {
   expect_is(resource()(), "tundraContainer")
+  expect_is(resource()(), "tundraContainer")
 })
+
+test_that("it can train a simple lm model", {
+  model <- resource()()
+  model$train(within(iris, dep_var <- as.integer(Sepal.Length > 5)))
+  expect_true(model$trained)
+})
+
+test_that("it can predict on a simple lm model", {
+  model <- resource()()
+  data <- within(iris, dep_var <- as.integer(Sepal.Length > 5))
+  model$train(data[-1])
+  expect_equal(stats::predict(lm(dep_var ~ ., data[-1])), model$predict(data[-1]))
+})
+
 
