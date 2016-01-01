f7d85a6bf03481c69bf117f62de01f0eb23d2714
RobertZK <technoguyrob@gmail.com>
2015-12-31 23:41:45 -0600

    Use mungebits2 in mungebits controller.

--- lib/controllers/mungebits.R
+++ lib/controllers/mungebits.R
@@ -2,6 +2,6 @@
 
 function(input, director) {
   simple_deflate <- director$resource("lib/shared/simple_deflate")
-  mungebits:::mungebit(simple_deflate(input$train), simple_deflate(input$predict))
+  mungebits2::mungebit$new(simple_deflate(input$train), simple_deflate(input$predict))
 }
 
