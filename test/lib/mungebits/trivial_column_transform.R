ad556ace855acb70ec8b06cfeec4012bd2c2dea2
RobertZK <technoguyrob@gmail.com>
2016-01-19 11:27:33 -0600

    Explicit exit with status on failure

--- test/lib/mungebits/trivial_column_transform.R
+++ test/lib/mungebits/trivial_column_transform.R
@@ -1,7 +1,6 @@
 test_that("it reverses some columns in the dataframe", {
   bit <- resource()
   iris2 <- iris
-  expect_true(false)
   iris2[1:2] <- lapply(iris[1:2], rev)
   expect_equal(bit$run(iris, 1:2), iris2)
   expect_equal(bit$run(iris, 1:2), iris2)
