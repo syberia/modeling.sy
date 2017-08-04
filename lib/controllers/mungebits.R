a86f4b2b583e6a4b09c75398d85114471b8d113b
peterhurford <peter@peterhurford.com>
2017-08-04 12:13:14 -0500

    Avoid invocation of mungebits

--- lib/controllers/mungebits.R
+++ lib/controllers/mungebits.R
@@ -1,12 +1,12 @@
 # This is the mungebits controller.
 
 function(input, director) {
-  simple_deflate <- director$resource("lib/shared/simple_deflate")
-  if (isTRUE(input$column_transformation)) {
-    mungebits2::mungebit$new(mungebits2::column_transformation(simple_deflate(input$train)),
-                             mungebits2::column_transformation(simple_deflate(input$predict)))
-  } else {
-    mungebits2::mungebit$new(simple_deflate(input$train), simple_deflate(input$predict))
-  }
+  # simple_deflate <- director$resource("lib/shared/simple_deflate")
+  # if (isTRUE(input$column_transformation)) {
+  #   mungebits2::mungebit$new(mungebits2::column_transformation(simple_deflate(input$train)),
+  #                            mungebits2::column_transformation(simple_deflate(input$predict)))
+  # } else {
+  #   mungebits2::mungebit$new(simple_deflate(input$train), simple_deflate(input$predict))
+  # }
 }
 
